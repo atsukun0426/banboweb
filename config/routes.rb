fa1671bb5a4d0444f9417e5846ea037db5b47881
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-10-06 15:49:06 +0900

    deviseでperfomer view controller model作成, organizer修正必要

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :performers, controllers: {
+    sessions: 'performers/sessions',
+    passwords: 'performers/sessions',
+    registrations: 'performers/registrations'
+  }
   namespace :banbo do
     get '/', to: 'home#top'
+    devise_for :organizers, path: :organizers, controllers: {
+    sessions: 'organizers/sessions',
+    passwords: 'organizers/passwords',
+    registrations: 'organizers/registrations'
+  }
   end
 end
