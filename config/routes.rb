a65e06f54d2756884e19172443a93fd66d9e83d5
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-10-06 16:03:33 +0900

    orgnizersを新しく作成 banboディレクトリに作成したorganizers削除必要

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :performers, controllers: {
-    sessions: 'performers/sessions',
-    passwords: 'performers/sessions',
+    sessions:      'performers/sessions',
+    passwords:     'performers/sessions',
     registrations: 'performers/registrations'
+  }
+  devise_for :organizers, path: :organizers, controllers: {
+    sessions:      'organizers/sessions',
+    passwords:     'organizers/passwords',
+    registrations: 'organizers/registrations'
   }
   namespace :banbo do
     get '/', to: 'home#top'
-    devise_for :organizers, path: :organizers, controllers: {
-    sessions: 'organizers/sessions',
-    passwords: 'organizers/passwords',
-    registrations: 'organizers/registrations'
-  }
   end
 end
