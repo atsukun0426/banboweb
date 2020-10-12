454c6368cc091297c8f9d5d6cd182cc18ea4b771
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-10-13 01:22:34 +0900

    メール認証機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :sign_in_required, only: [:show]
   def show
     @user = current_user
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/', to: 'home#top'
+  root to: 'home#top'
 
   devise_for :users, controllers: {
     sessions:      'users/sessions',
