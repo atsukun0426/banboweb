0542e5e6e327f4278a0c66ca7cebf096776b8d38
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-10-12 23:35:09 +0900

    organizer perfomer削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,11 @@
 Rails.application.routes.draw do
-  #get '/', to: 'home#top'
+  get '/', to: 'home#top'
+
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
     registrations: 'users/registrations'
   }
-  resources :users, only: [:show]
 
-  devise_for :performers, controllers: {
-    sessions:      'performers/sessions',
-    passwords:     'performers/passwords',
-    registrations: 'performers/registrations'
-  }
-  devise_for :organizers, path: :organizers, controllers: {
-    sessions:      'organizers/sessions',
-    passwords:     'organizers/passwords',
-    registrations: 'organizers/registrations'
-  }
-  namespace :banbo do
-    get '/', to: 'home#top'
-  end
+  resources :users, only: [:show]
 end
