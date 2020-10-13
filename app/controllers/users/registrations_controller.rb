d44251ef4074d17efc2f356e72b8351114f88ebd
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-10-14 00:21:48 +0900

    プロフィール編集後、ユーザーページにリダイレクト

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  protected
+    def after_update_path_for(resource)
+      user_path(id: current_user.id)
+    end
   # GET /resource/sign_up
   # def new
   #   super
@@ -39,6 +42,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
