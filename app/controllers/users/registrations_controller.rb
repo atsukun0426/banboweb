19326d669a8b407076edb87bfbacacf4b2f4f635
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-10-14 00:31:25 +0900

    パスワードなしでプロフィール編集

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   protected
+
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+
     def after_update_path_for(resource)
       user_path(id: current_user.id)
     end
