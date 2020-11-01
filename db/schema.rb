0b8b9b9f8cbc57b427006b07bdbb0e3370a25a06
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-11-01 17:20:57 +0900

    postモデルにtitle追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_075259) do
+ActiveRecord::Schema.define(version: 2020_11_01_081023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,12 +68,12 @@ ActiveRecord::Schema.define(version: 2020_11_01_075259) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.text "text"
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.text "content"
+    t.string "title"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
   end
 
