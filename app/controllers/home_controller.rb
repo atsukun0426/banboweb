45b1232bdebbcd05087f400840b9675970b6527c
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-11-07 23:02:43 +0900

    トップページに募集一覧実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def top
+    @recruitments = Recruitment.oall.order(created_at: :desc)
+    @recruitments = Recruitment.page(params[:page]).per(20).order('updated_at DESC')
   end
 end
