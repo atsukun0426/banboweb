05153b5e4fd81e0f89f2400ec8cc73e22cc1fabd
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-11-16 16:39:26 +0900

    pry追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  require 'pry'
   before_action :sign_in_required, only: [:show]
   def index
-    @users = User.all
+    @search_params = user_search_params
+    @users = User.search(@search_params).includes(:prefecture)
+    #@users = User.all
+    #@users = User.where(activated: true).page(params[:page]).search(params[:search])
   end
 
   def show
@@ -9,6 +13,10 @@ class UsersController < ApplicationController
     @posts = @user.posts.page(params[:page]).per(20)
   end
 
+  private
+  def user_search_params
+    params.fetch(:search, {}).permit(:name, :prefecture_id)
+  end
   # def users_posts
   # @user = User.find(params[:id])
   # @posts = Post.where(user_id: @user.id).all.order("created_at DESC").page(params[:page]).per(20)
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,26 @@ class User < ApplicationRecord
   has_many :chats
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
   validates :name, presence: true
   validates :description, length: { maximum: 200 }
+
+  scope :search, -> (search_params) do
+    return if search_params.blank?
+
+    name_like(search_params[:name])
+      .prefecture_id_is(search_params[:prefecture_id])
+  end
+
+  scope :name_like, -> (name) { where('name LIKE ?', "%#{name}%") if name.present? }
+  scope :prefecture_id_is, -> (prefecture_id) { where(prefecture_id: prefecture_id) if prefecture_id.present? }
+  #ここでのself.はUser.を意味する
+  def self.search(search)
+    if search
+      where(['name LIKE ?', "%#{search}%"]) #検索とnameの部分一致を表示。User.は省略
+    else
+      all #全て表示。User.は省略
+    end
+  end
 
   def self.from_omniauth(auth)
     where(uid: auth.uid).first
