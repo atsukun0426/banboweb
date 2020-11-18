130868bfac829ddcb7779467664f691c5cf86a98
岩崎敦基 <iwasakiatsuki@iwasakiatsushimotonoMacBook-Pro.local>
2020-11-18 18:08:21 +0900

    rubocop適用

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  # belongs_to :organizer
   has_many :comments
-  default_scope -> { order(created_at: :desc) }
+  # default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -2,9 +2,8 @@ class Recruitment < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user
-  # belongs_to :organizer
   has_many :requests
-  default_scope -> { order(created_at: :desc) }
+  # default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 500 }
 
